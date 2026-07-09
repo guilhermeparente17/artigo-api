682273606c66dd2944ddaaecae130a79467eeff3
guilherme parente <guilherme.parente17@outlook.com>
2026-07-08 21:21:15 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
-
   app.enableVersioning({
     type: VersioningType.URI,
   });
